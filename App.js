c3b508fc47435f92b1a55e58f6e8f944f673ed79
joblulhoque <55913362+joblulhoque@users.noreply.github.com>
2021-08-01 21:39:46 +0200

    Add files via upload
    creating a clock with useeffect from udemy hooks course.

--- App.js
+++ App.js
@@ -1,45 +1,27 @@
 import react, {useState, useEffect} from 'react';
 import './App.css';
 
-let born = false;
+
 function App() {
+ const [time,setTime] = useState(Date);
+  
+ useEffect( () => {
 
-  const [growth, setGrowth] = useState(0);
-  const [nirvana, setNirvana] = useState(false);
+   const handle = setInterval(()=>{
+      setTime(Date);
+   },1000);
 
-  useEffect( () => {
-    if(born){
-      document.title = "nirvana attained";
-    }
-  },[nirvana]);
-
-  useEffect( () => {
-    console.log("I am born.");
-  },[]);
-
-  useEffect( () => {
-   if(born){
-     console.log(" make mistake and learn");
-   } else {
-     born = true;
+   return function () {
+     clearInterval(handle);
    }
-   if(growth > 70){
-     setNirvana(true);
-   }
-   return function cleanup() {
-     console.log("cleanup after mistakes.");
-   }
-  });
 
-  function growHandle () {
-    setGrowth( growth + 10);
-  }
+ });
 
   return (
     <div className="App">
-      <h1>Use Effect.</h1>
-      <h3>Growth: {growth}</h3>
-      <button onClick={growHandle}>learn and grow</button>
+      <h1>Use Effect example </h1>
+      <h3>Date and time : {time}</h3>
+      
     </div>
   );
 }
