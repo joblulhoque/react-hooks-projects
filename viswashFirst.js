81f9492f3e547a5d6ad188ee7a025a007d3dcf5f
joblulhoque <55913362+joblulhoque@users.noreply.github.com>
2021-08-02 15:56:20 +0200

    Add files via upload

---
+++ viswashFirst.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class ClassCounterOne extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    componentDidMount(){
+        document.title = `Clicked ${this.state.count} times`;
+    }
+    componentDidUpdate(){
+        document.title = `Clicked ${this.state.count} times`;
+    }
+
+    render(){
+        return (
+            <div>
+            <button onClick={()=>this.setState({count:this.state.count + 1})}> click {this.state.count} times</button>
+            </div>
+        )
+    }
+
+}
+export default ClassCounterOne;
